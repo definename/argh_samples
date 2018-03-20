358ad800ba08b2a70674be1c77075c00a6fa4948
Oleg Kolomiytsev <kolomiytsev.oleg@gmail.com>
2018-03-20 23:06:46 +0200

    Argh library completely tested.

--- modules/argh-test/src/main.cpp
+++ modules/argh-test/src/main.cpp
@@ -1,29 +1,56 @@
 #include "pch.h"
 
 /*
-Usage:
-    --call out_offers_14_15_new.log --timeout=500 --index=24533 --delay=500
+    Usage:
+        --call --file=out_offers_14_15_new.log --timeout=500 --index=24533 --delay=500 --thread=1
+        --call --file out_offers_14_15_new.log --timeout 500 --index 24533 --delay 500 --thread 1
 */
 
 int main(int argc, char* argv[])
 {
     try
     {
+
+        // To parse: --call --file=out_offers_14_15_new.log --timeout=500 --index=24533 --delay=500 --thread=1
         auto cmdl = argh::parser(argc, argv);
 
+        // To parse: --call --file out_offers_14_15_new.log --timeout 500 --index 24533 --delay 500 --thread 1
+        //auto cmdl = argh::parser(argc, argv, argh::parser::PREFER_PARAM_FOR_UNREG_OPTION);
+
         if (cmdl[{"-c", "--call"}]) {
+
+            std::string file(cmdl({ "-f", "--file" }).str());
+
             unsigned int threadCount;
             cmdl({ "-t", "--thread" }, 1) >> threadCount;
-            unsigned int delay;
-            cmdl({ "-d", "--delay" }, 0) >> delay;
             unsigned int startIndex;
             cmdl({ "-i", "--index" }, 1) >> startIndex;
+            unsigned int delay;
+            cmdl({ "-d", "--delay" }, 0) >> delay;
 
-            std::cout << "call: "<< cmdl[1] 
-                << " thread:" << threadCount
-                << " delay:" << delay
-                << " startIndex: " << startIndex << std::endl;
+            unsigned int timeout;
+            if (!(cmdl({ "-t", "--timeout" }) >> timeout))
+                throw std::runtime_error("Invalid timeout was given");
+
+            std::cout
+                << "file: " << file << "\n"
+                << "thread:" << threadCount << "\n"
+                << "startIndex: " << startIndex << "\n"
+                << "delay:" << delay << "\n"
+                << "timeout:" << timeout << "\n";
         }
+
+        std::cout << "Positional args:\n";
+        for (auto& pos_arg : cmdl.pos_args())
+            std::cout << '\t' << pos_arg << '\n';
+
+        std::cout << "\nFlags:\n";
+        for (auto& flag : cmdl.flags())
+            std::cout << '\t' << flag << '\n';
+
+        std::cout << "\nParameters:\n";
+        for (auto& param : cmdl.params())
+            std::cout << '\t' << param.first << " : " << param.second << '\n';
     }
     catch (const std::exception& e)
     {
